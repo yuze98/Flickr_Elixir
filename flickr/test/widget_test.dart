3d4110f8e060976eb2e6a135c7d9397b3a530930
Youssef Hatem <65419885+yuze98@users.noreply.github.com>
2021-04-24 00:55:44 +0200

    Update widget_test.dart

--- flickr/test/widget_test.dart
+++ flickr/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flickr/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flickr/Screens/Login_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(LoginScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
