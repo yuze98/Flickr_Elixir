fc290f5e57ab81ee1ed80f4c1042f6b05bff187a
Youssef Hatem <65419885+yuze98@users.noreply.github.com>
2021-04-24 00:47:33 +0200

    call from main

--- flickr/lib/Screens/Login_page.dart
+++ flickr/lib/Screens/Login_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class LoginScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- flickr/lib/main.dart
+++ flickr/lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Screens/Login_page.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flickr',
-      home: Scaffold(
-        appBar: AppBar(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(30),
-            ),
-          ),
-          title: Title(
-            child: Text("Flicker"),
-            color: Colors.blue,
-          ),
-        ),
-        body: Container(
-          color: Colors.redAccent[400],
-        ),
-      ),
-    );
-  }
+  runApp(LoginScreen());
 }
