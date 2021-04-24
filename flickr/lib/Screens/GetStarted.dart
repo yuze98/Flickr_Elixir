1e3dd98820fd68dc608c4b3749483b7ccef4b0ab
Fat7y99 <67173807+Fat7y99@users.noreply.github.com>
2021-04-24 23:35:44 +0200

    Added GetStarted

---
+++ flickr/lib/Screens/GetStarted.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flickr',
+      home: Scaffold(
+        appBar: AppBar(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(30),
+            ),
+          ),
+          title: Title(
+            child: Text("Flicker"),
+            color: Colors.blue,
+          ),
+        ),
+        body: Container(
+          color: Colors.redAccent[400],
+        ),
+      ),
+    );
+  }
+}
