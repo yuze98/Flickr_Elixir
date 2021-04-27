00f5f576a9b50629ba51caa2abdcdb0098e664c5
Youssef Hatem <65419885+yuze98@users.noreply.github.com>
2021-04-27 04:50:04 +0200

    Update SignUp.dart

--- flickr/lib/Screens/SignUp.dart
+++ flickr/lib/Screens/SignUp.dart
@@ -1,122 +1,180 @@
 import 'package:flutter/material.dart';
 
-class Signup extends StatelessWidget {
-  // This widget is the root of your application.
+class Signup extends StatefulWidget {
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  final firstnameController = TextEditingController();
+  final secondnameController = TextEditingController();
+  final ageController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    var devicesize = MediaQuery.of(context).size;
+
     return MaterialApp(
       title: 'Flickr',
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.black,
-          leading: Image.asset('images/flickricon.png'),
+          leading: Image.asset(
+            'images/flickricon.png',
+            scale: devicesize.width * 0.03,
+          ),
           title: Text(
-            'Flickr',
+            'flickr',
             style: TextStyle(
-              fontSize: 25,
+              fontSize: devicesize.width * 0.07,
               fontWeight: FontWeight.bold,
             ),
           ),
-          toolbarHeight: 100,
+          toolbarHeight: devicesize.height * 0.15,
+          actions: [
+            IconButton(
+                icon: Icon(Icons.view_quilt),
+                onPressed: () {
+                  Navigator.pop(context);
+                })
+          ],
         ),
-        body: ListView(
-          children: [
-            SizedBox(
-              height: 25.0,
-            ),
-            Image.asset(
-              'images/flickricon.png',
-              height: 35,
-            ),
-            Center(
-              child: Text(
-                'Sign up for Flickr',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.normal,
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(
+              devicesize.width * 0.06, 0, devicesize.width * 0.06, 0),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 25.0,
+              ),
+              Image.asset(
+                'images/flickricon.png',
+                height: devicesize.height * 0.04,
+              ),
+              Center(
+                child: Text(
+                  'Sign up for Flickr',
+                  style: TextStyle(
+                    fontSize: devicesize.width * 0.06,
+                    fontWeight: FontWeight.normal,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(25, 20, 25, 5),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  contentPadding: EdgeInsets.all(25),
-                  border: OutlineInputBorder(),
-                  hintText: 'First name',
-                  fillColor: Colors.white,
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01,
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01),
+                child: TextField(
+                  controller: firstnameController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: 'First name',
+                    fillColor: Colors.white,
+                  ),
+                  //  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  contentPadding: EdgeInsets.all(25),
-                  border: OutlineInputBorder(),
-                  hintText: 'Second name',
-                  fillColor: Colors.white,
+              ), //first name
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                child: TextField(
+                  controller: secondnameController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: 'Second name',
+                    fillColor: Colors.white,
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  contentPadding: EdgeInsets.all(25),
-                  border: OutlineInputBorder(),
-                  hintText: 'Your age',
-                  fillColor: Colors.white,
+              ), // second name
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01,
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01),
+                child: TextField(
+                  controller: ageController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: 'Your age',
+                    fillColor: Colors.white,
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  contentPadding: EdgeInsets.all(25),
-                  border: OutlineInputBorder(),
-                  hintText: 'Email address',
-                  fillColor: Colors.white,
+              ), //your age
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01,
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: 'Email address',
+                    fillColor: Colors.white,
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  contentPadding: EdgeInsets.all(25),
-                  border: OutlineInputBorder(),
-                  hintText: 'Password',
-                  fillColor: Colors.white,
+              ), //email address
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01,
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: 'Password',
+                    fillColor: Colors.white,
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              height: 130.0,
-              child: Padding(
-                padding: const EdgeInsets.all(30.0),
+              ), // password
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.07,
+                    devicesize.width * 0.06,
+                    devicesize.height * 0.01),
                 child: TextButton(
                   style: TextButton.styleFrom(
                     primary: Colors.white,
                     backgroundColor: Colors.blue,
                     shape: const BeveledRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                    minimumSize: Size(devicesize.width, devicesize.height / 8),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    print(firstnameController.text);
+                    print(secondnameController.text);
+                    print(ageController.text);
+                    print(emailController.text);
+                    print(passwordController.text);
+
+                    //Navigator.pop(context);
+                  },
                   child: Text(
                     "Sign up",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: devicesize.width * 0.06),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
         backgroundColor: Colors.white,
       ),
